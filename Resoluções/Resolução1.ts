7a3d631ca514da950969081409ba8fd97bd501d0
felipe <felipe.dreis.monteiro@gmail.com>
2022-05-26 23:29:10 -0300

    Desafios typeScript

---
+++ Resoluções/Resolução1.ts
@@ -0,0 +1,7 @@
+interface employee {
+    name: String;
+    code: Number;
+}
+let employee = {} as employee;
+employee.code = 10;
+employee.name = "John";
---
+++ Resoluções/Resolução2.ts
@@ -0,0 +1,30 @@
+// Como podemos melhorar o esse código usando TS? 
+
+enum Profissao {
+    Padeiro,
+    Atriz
+}
+
+let pessoa1 = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Profissao.Padeiro,
+}
+
+let pessoa2 = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Profissao.Padeiro,
+}
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: Profissao.Atriz
+};
+
+let pessoa4 = {
+    nome:"carlos",
+    idade:19,
+    profissao:Profissao.Padeiro
+}
